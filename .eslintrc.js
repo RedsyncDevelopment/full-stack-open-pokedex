5d52449bb0389c9438fec275842de1881014227c
[Ivan Horvat] <development@redsync.studio>
2022-11-21 12:53:13 +0100

    Fixing more lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,7 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    quotes: ["error", "double"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
